ebf649902a83a2d962dcfdf037c84a03cbbb34bb
Lidstrom Olof <olof.lidstrom@tieto.com>
2017-08-25 12:56:33 +0200

    Add project files.

---
+++ Ollzi.ImageService/IImageService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace Ollzi.ImageService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IImageService
+    {
+        [OperationContract]
+        IEnumerable<ImageMetaData> GetImages();
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class ImageMetaData
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
